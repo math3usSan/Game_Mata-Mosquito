0db9704f874ae8045f9a184a3cadba89cbd2ee5a
Matheus Santos <111880593+math3usSan@users.noreply.github.com>
2023-01-12 10:34:37 -0300

    timer do mosquito

--- jogo.js
+++ jogo.js
@@ -15,6 +15,10 @@ ajustaTamanhoPalcoJogo()
 
 function posicaoRandomica() {
 
+    if(document.getElementById('mosquito')) {
+        document.getElementById('mosquito').remove()
+    }
+
     var posicaoX = Math.floor(Math.random() * largura) - 90
     var posicaoY = Math.floor(Math.random() * altura) - 90
 
@@ -31,6 +35,7 @@ function posicaoRandomica() {
     mosquito.style.left = posicaoX + 'px'
     mosquito.style.top = posicaoY + 'px'
     mosquito.style.position = 'absolute'
+    mosquito.id = 'mosquito'
 
     document.body.appendChild(mosquito)
 }
