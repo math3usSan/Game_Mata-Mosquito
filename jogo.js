c59720222df9d0b7f1b305355f94c07c1c5bb17b
Matheus Santos <111880593+math3usSan@users.noreply.github.com>
2023-01-17 11:10:58 -0300

    tela fim de jogo

--- jogo.js
+++ jogo.js
@@ -1,6 +1,7 @@
 
 var altura = 0
 var largura = 0
+var vidas = 1
 
 function ajustaTamanhoPalcoJogo() {
 
@@ -17,6 +18,15 @@ function posicaoRandomica() {
 
     if(document.getElementById('mosquito')) {
         document.getElementById('mosquito').remove()
+
+        if (vidas > 3){
+            window.location.href = 'fim_de_jogo.html'
+
+        } else {
+            document.getElementById('v' + vidas).src="imagens/coracao_vazio.png"
+            
+            vidas++
+            }
     }
 
     var posicaoX = Math.floor(Math.random() * largura) - 90
@@ -36,6 +46,9 @@ function posicaoRandomica() {
     mosquito.style.top = posicaoY + 'px'
     mosquito.style.position = 'absolute'
     mosquito.id = 'mosquito'
+    mosquito.onclick = function() {
+        this.remove()
+    }
 
     document.body.appendChild(mosquito)
 }
